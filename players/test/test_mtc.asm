35b9e9444a91f06266773ab650f00b34395ff4c3
vitamin-caig <vitamin.caig@gmail.com>
2015-05-20 15:31:11 +0300

    players: initial version of PT2/PT3/TS/MTC/TFC player.

---
+++ players/test/test_mtc.asm
@@ -0,0 +1,23 @@
+      device zxspectrum128
+
+      org #6000
+begin 
+      include "test.asm"
+      
+      org #8000
+player      
+init=player
+play=player+5
+mute=player+8
+      include "mtc.asm"
+
+module      
+      ;incbin MTC_CYBERMOTION
+      ;incbin MTC_DRUMTEST
+      ;incbin MTC_MUG
+      incbin MTC_MYSTICAL
+      ;incbin MTC_NEWOLD
+      ;incbin MTC_SNEGURKA
+
+      savesna "test_mtc.sna",begin
+      savebin "../mtc_8000.bin",player,module-player
